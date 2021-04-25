42512582de8056171b503de1e688fb0c61640728
roy-a2yush <roy.a2yush@gmail.com>
2021-04-25 23:59:51 +0530

    Added mongoDB atlas connection

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+const connectDB = require('./config/db')
+
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
 // All the base routes here
- app.use('/provider', require('./routes/provider'))
+app.use('/provider', require('./routes/provider'))
+
+connectDB()
 
 const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
 
-module.exports = index
+// module.exports = index
